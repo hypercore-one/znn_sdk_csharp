59c5bb21508aa50857c2332b3c23c2fc865460b7
CryptoFish <YankeeDoodlePigeon@protonmail.com>
2023-07-17 16:07:58 +0200

    Add Extract and AddDecimals methods

--- src/Zenon/Utils/AmountUtils.cs
+++ src/Zenon/Utils/AmountUtils.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Globalization;
+using System.Numerics;
 
 namespace Zenon.Utils
 {
@@ -7,6 +8,46 @@ namespace Zenon.Utils
         public static BigInteger ParseAmount(string value)
         {
             return string.IsNullOrEmpty(value) ? BigInteger.Zero : BigInteger.Parse(value);
+        }
+
+        public static BigInteger ExtractDecimals(double value, int decimals)
+        {
+            return BigInteger.Parse(value.ToString("0." + new string('0', decimals))
+                .Replace(CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator, ""));
+        }
+
+        public static string AddDecimals(BigInteger value, int decimals)
+        {
+            return CreateAndStripZerosForScale(value, decimals, 0);
+        }
+
+        public static string CreateAndStripZerosForScale(
+            BigInteger intVal,
+            int scale,
+            int preferredScale)
+        {
+            var ten = new BigInteger(10);
+
+            while (intVal.CompareTo(ten) >= 0 && scale > preferredScale)
+            {
+                if (!intVal.IsEven)
+                {
+                    break;
+                }
+                var remainder = BigInteger.Remainder(intVal, ten);
+
+                if (remainder.Sign != 0)
+                {
+                    break;
+                }
+                intVal = BigInteger.Divide(intVal, ten);
+                scale += -1;
+            }
+
+            var strVal = intVal.ToString();
+            return scale > 0 
+                ? strVal.Insert(strVal.Length - scale, CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator)
+                : strVal;
         }
     }
 }
