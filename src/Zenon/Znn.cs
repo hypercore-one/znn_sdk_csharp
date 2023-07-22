24c9a64c16eccb6211d9b26e2e4ba2a581f1e215
CryptoFish <YankeeDoodlePigeon@protonmail.com>
2023-07-22 19:33:22 +0200

    Add static Mainnet property

--- src/Zenon/Znn.cs
+++ src/Zenon/Znn.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Principal;
 using System.Threading.Tasks;
 using Zenon.Client;
 using Zenon.Model.NoM;
@@ -9,27 +8,13 @@ using Zenon.Wallet;
 
 namespace Zenon
 {
-    public class Znn
+    public sealed class Znn
     {
-        public int ChainIdentifier { get; }
-
-        public IWalletAccount DefaultWalletAccount { get; set; }
-
-        public Lazy<IClient> Client { get; }
-
-        public Api.LedgerApi Ledger { get; }
-        public Api.StatsApi Stats { get; }
-        public Api.EmbeddedApi Embedded { get; }
-        public Api.SubscribeApi Subscribe { get; }
-
-        public Znn()
-            : this(Constants.ChainId)
-        { }
+        public static readonly Znn Mainnet = new Znn(Constants.ChainId);
 
         public Znn(int chainIdentifier)
         {
             ChainIdentifier = chainIdentifier;
-
             Client = new Lazy<IClient>(() => new WsClient());
             Ledger = new Api.LedgerApi(Client);
             Stats = new Api.StatsApi(Client);
@@ -37,7 +22,16 @@ namespace Zenon
             Subscribe = new Api.SubscribeApi(Client);
         }
 
-        public async Task<AccountBlockTemplate> SendAsync(AccountBlockTemplate transaction, 
+        public IWalletAccount DefaultWalletAccount { get; set; }
+
+        public int ChainIdentifier { get; }
+        public Lazy<IClient> Client { get; }
+        public Api.LedgerApi Ledger { get; }
+        public Api.StatsApi Stats { get; }
+        public Api.EmbeddedApi Embedded { get; }
+        public Api.SubscribeApi Subscribe { get; }
+
+        public async Task<AccountBlockTemplate> SendAsync(AccountBlockTemplate transaction,
             Action<PowStatus> generatingPowCallback = default, bool waitForRequiredPlasma = false)
         {
             return await SendAsync(transaction, DefaultWalletAccount, generatingPowCallback, waitForRequiredPlasma);
